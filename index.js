1599916b8a2e71b03f6cea824ea31c06f025211a
Edwin <edwin.h.morris@gmail.com>
2024-08-01 12:08:29 -0400

    Font, random letter on space, and more

--- index.js
+++ index.js
@@ -14,55 +14,78 @@ const [CTX, canvasWidth, canvasHeight] = generateCanvas({
 });
 
 let textString = "A";
+let textType = "letter";
 let lastLetterUpdate = Date.now();
-let lastKeyPress = Date.now();
+const initTime = Date.now();
 
-animate((deltaTime) => {
-  CTX.clearRect(0, 0, canvasWidth, canvasHeight);
-
+const drawGuides = (CTX) => {
+  CTX.save();
   CTX.fillStyle = "red";
   CTX.fillRect(canvasWidth / 2, 0, 1, canvasHeight);
   CTX.fillRect(0, canvasHeight / 2, canvasWidth, 1);
   CTX.fillStyle = "black";
-
-  CTX.save();
-  const timeTransitionSize = progress(0, 1600, Date.now() - lastKeyPress);
-  const timeTransitionRotate = progress(0, 1900, Date.now() - lastKeyPress);
-
-  const fontSizeTransition = transition(
-    97,
-    103,
-    timeTransitionSize,
-    easeInOutSine
-  );
-  const angleTransition = transition(
-    degToRag(-2),
-    degToRag(2),
-    timeTransitionRotate,
-    easeInOutSine
-  );
-
-  CTX.font = `600 ${fontSizeTransition}vmin -apple-system, BlinkMacSystemFont`;
-  CTX.textAlign = "center";
-
-  const measurement = CTX.measureText(textString);
-  const actualHeight =
-    measurement.actualBoundingBoxAscent + measurement.actualBoundingBoxDescent;
-
-  CTX.translate(canvasWidth / 2, canvasHeight / 2);
-  CTX.rotate(angleTransition);
-  CTX.fillText(textString, 0, actualHeight / 2);
   CTX.restore();
-});
+};
+
+const updateText = (newText) => {
+  textString = newText.toUpperCase();
+  textType = /[a-zA-Z]/.test(newText) ? "letter" : "number";
+  lastLetterUpdate = Date.now();
+};
+
+const setRandomText = () => {
+  const options = "ABCDEFGHIJKLMNOPQRSTUVQXYZ0123456789";
+  updateText(options.split("")[Math.floor(Math.random() * options.length)]);
+};
 
 document.addEventListener("keypress", ({ key }) => {
-  lastKeyPress = Date.now();
+  console.log(key);
+  const debounceTime = 0;
   if (
-    Date.now() - lastLetterUpdate > 500 &&
+    Date.now() - lastLetterUpdate > debounceTime &&
     key.length === 1 &&
     /[a-zA-Z0-9]/.test(key)
   ) {
-    textString = key.toUpperCase();
-    lastLetterUpdate = Date.now();
+    updateText(key);
+  } else if (key === " ") {
+    setRandomText();
   }
+});
+
+new FontFace("Ginto", "url(./Ginto.woff2)").load().then((font) => {
+  document.fonts.add(font);
+
+  animate((deltaTime) => {
+    CTX.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    drawGuides(CTX);
+
+    CTX.save();
+    const timeTransitionSize = progress(0, 1600, Date.now() - initTime);
+    const timeTransitionRotate = progress(0, 1900, Date.now() - initTime);
+
+    const fontSizeTransition = transition(
+      97,
+      103,
+      timeTransitionSize,
+      easeInOutSine
+    );
+    const angleTransition = transition(
+      degToRag(-2),
+      degToRag(2),
+      timeTransitionRotate,
+      easeInOutSine
+    );
+
+    CTX.font = `600 ${fontSizeTransition}vmin Ginto`;
+    CTX.textAlign = "center";
+    CTX.textBaseline = "middle";
+    const verticalOffset = CTX.measureText(textString).width / 8;
+    CTX.translate(canvasWidth / 2, canvasHeight / 2 + verticalOffset);
+    CTX.rotate(angleTransition);
+    CTX.fillStyle = textType === "letter" ? "blue" : "red";
+    CTX.fillText(textString, 0, 0);
+
+    CTX.restore();
+  });
 });
